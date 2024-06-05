f69496361b03b7b7b95b6c627af22a99b1c006d7
Mohammad Zbeeb <mhmdzbyb829@gmail.com>
2024-06-05 10:04:55 +0300

    removed prints

--- pj2/driver.py
+++ pj2/driver.py
@@ -3,12 +3,6 @@ from spacy_extractor import SpacyExtractor
 import extract_msg
 import os      
 a = Main("/home/mohammad/Desktop/pj2/data","/home/mohammad/Desktop/pj2/data/output.csv","sales")    
-# example = "/home/mohammad/Desktop/pj1/test/Levon _ I hope you don't mind me reaching out to you_.msg"
 a.extract() 
-# mymsg = extract_msg.Message(example) 
-# print(mymsg.sender) 
-# print(mymsg.recipients) 
-# print(mymsg.subject)
-# print(mymsg.date) 
-# print(mymsg.body)
+
  
